7571c3bbc59f58a3d0574bec4b74a62742bf316a
赵九州 <2426047957@qq.com>
2020-08-19 09:22:30 +0800

    项目创建完成

---
+++ src/test/java/com/edu/linkconvert/LinkconvertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.linkconvert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkconvertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
